28ba66bfd09598c901c32dbce04f25940c29fde1
Zach Allen <zachallen@texasmutual.com>
2024-03-03 11:10:31 -0600

    Add working json deserialization to class

--- iRacingData/iRacingData/Program.cs
+++ iRacingData/iRacingData/Program.cs
@@ -1,1 +1,8 @@
-﻿Console.WriteLine("Hello, World!");
+﻿using iRacingData;
+using System.Text.Json;
+
+var JsonResponse = File.ReadAllText(@"C:\Users\zcall\car.json");
+Console.WriteLine("Hello, World!");
+var car = JsonSerializer.Deserialize<iRacingCar>(JsonResponse);
+
+Console.ReadKey();
--- iRacingData/iRacingData/iRacingCar.cs
+++ iRacingData/iRacingData/iRacingCar.cs
@@ -7,48 +7,76 @@ using System.Threading.Tasks;
 
 namespace iRacingData
 {
+    [JsonUnmappedMemberHandling(JsonUnmappedMemberHandling.Disallow)]
     internal class iRacingCar
     {
+        [JsonPropertyName("ai_enabled")]
+        public bool AiEnabled { get; set; }
+
+        [JsonPropertyName("allow_number_colors")]
+        public bool AllowNumberColors { get; set; }
+        [JsonPropertyName("allow_number_font")]
+        public bool AllowNumberFont { get; set; }
+        [JsonPropertyName("allow_sponsor1")]
+        public bool AllowSponsor1 { get; set; }
+        [JsonPropertyName("allow_sponsor2")]
+        public bool AllowSponsor2 { get; set; }
+        [JsonPropertyName("allow_wheel_color")]
+        public bool AllowWheelColor { get; set; }
+        [JsonPropertyName("award_exempt")]
+        public bool AwardExempt { get; set; }
+        [JsonPropertyName("car_dirpath")]
+        public string CarDirpath { get; set; }
+        [JsonPropertyName("car_id")]
+        public int CarId { get; set; }
+        [JsonPropertyName("car_name")]
+        public string CarName { get; set; }
+        [JsonPropertyName("car_name_abbreviated")]
+        public string CarNameAbbreviated { get; set; }
+        [JsonPropertyName("car_types")]
+        public System.Text.Json.JsonElement CarTypes { get; set; }
+        //public IReadOnlyList<iRacingCar> CarTypes { get; set; }
+        [JsonPropertyName("car_weight")]
+        public int CarWeight { get; set; }
+        [JsonPropertyName("categories")]
+        public IReadOnlyList<string> Categories { get; set; }
+        [JsonPropertyName("created")]
+        public DateTime Created { get; set; }
+        [JsonPropertyName("first_sale")]
+        public DateTime FirstSale { get; set; }
+        [JsonPropertyName("forum_url")]
+        public string ForumUrl { get; set; }
+        [JsonPropertyName("free_with_subscription")]
+        public bool FreeWithSubscription { get; set; }
+        [JsonPropertyName("has_headlights")]
+        public bool HasHeadLights { get; set; }
+        [JsonPropertyName("has_multiple_dry_tire_types")]
+        public bool HasMultipleDryTireTypes { get; set; }
+        [JsonPropertyName("hp")]
+        public int Hp { get; set; }
+        [JsonPropertyName("is_ps_purchasable")]
+        public bool IsPsPurchasable { get; set; }
+        [JsonPropertyName("max_power_adjust_pct")]
+        public int MaxPowerAdjustPct { get; set; }
+        [JsonPropertyName("max_weight_penalty_kg")]
+        public int MaxWeightPenaltyKg { get; set; }
+        [JsonPropertyName("min_power_adjust_pct")]
+        public int MinPowerAdjustPct { get; set; }
+        [JsonPropertyName("package_id")]
+        public int PackageId { get; set; }
+        [JsonPropertyName("patterns")]
+        public int Patterns { get; set; }
+        [JsonPropertyName("price")]
+        public double Price { get; set; }
+        [JsonPropertyName("price_display")]
+        public string PriceDisplay { get; set; }
+        [JsonPropertyName("retired")]
+        public bool Retired { get; set; }
+        [JsonPropertyName("search_filters")]
+        public string SearchFilters { get; set; }
+        [JsonPropertyName("sku")]
+        public int Sku { get; set; }
         // Root myDeserializedClass = JsonSerializer.Deserialize<Root>(myJsonResponse);
-        public record iRacingCarRecord(
-            [property: JsonPropertyName("car_type")] string CarType
-        );
-
-        public record Root(
-            [property: JsonPropertyName("ai_enabled")] bool AiEnabled,
-            [property: JsonPropertyName("allow_number_colors")] bool AllowNumberColors,
-            [property: JsonPropertyName("allow_number_font")] bool AllowNumberFont,
-            [property: JsonPropertyName("allow_sponsor1")] bool AllowSponsor1,
-            [property: JsonPropertyName("allow_sponsor2")] bool AllowSponsor2,
-            [property: JsonPropertyName("allow_wheel_color")] bool AllowWheelColor,
-            [property: JsonPropertyName("award_exempt")] bool AwardExempt,
-            [property: JsonPropertyName("car_dirpath")] string CarDirpath,
-            [property: JsonPropertyName("car_id")] int CarId,
-            [property: JsonPropertyName("car_name")] string CarName,
-            [property: JsonPropertyName("car_name_abbreviated")] string CarNameAbbreviated,
-            [property: JsonPropertyName("car_types")] IReadOnlyList<iRacingCar> CarTypes,
-            [property: JsonPropertyName("car_weight")] int CarWeight,
-            [property: JsonPropertyName("categories")] IReadOnlyList<string> Categories,
-            [property: JsonPropertyName("created")] DateTime Created,
-            [property: JsonPropertyName("first_sale")] DateTime FirstSale,
-            [property: JsonPropertyName("forum_url")] string ForumUrl,
-            [property: JsonPropertyName("free_with_subscription")] bool FreeWithSubscription,
-            [property: JsonPropertyName("has_headlights")] bool HasHeadlights,
-            [property: JsonPropertyName("has_multiple_dry_tire_types")] bool HasMultipleDryTireTypes,
-            [property: JsonPropertyName("hp")] int Hp,
-            [property: JsonPropertyName("is_ps_purchasable")] bool IsPsPurchasable,
-            [property: JsonPropertyName("max_power_adjust_pct")] int MaxPowerAdjustPct,
-            [property: JsonPropertyName("max_weight_penalty_kg")] int MaxWeightPenaltyKg,
-            [property: JsonPropertyName("min_power_adjust_pct")] int MinPowerAdjustPct,
-            [property: JsonPropertyName("package_id")] int PackageId,
-            [property: JsonPropertyName("patterns")] int Patterns,
-            [property: JsonPropertyName("price")] double Price,
-            [property: JsonPropertyName("price_display")] string PriceDisplay,
-            [property: JsonPropertyName("retired")] bool Retired,
-            [property: JsonPropertyName("search_filters")] string SearchFilters,
-            [property: JsonPropertyName("sku")] int Sku
-        );
-
 
     }
 }

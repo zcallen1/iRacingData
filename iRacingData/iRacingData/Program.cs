0c91acf38b58da2aabc959497f6345cbd5d0003c
Zach Allen <zachallen@texasmutual.com>
2024-03-04 18:44:07 -0600

    Add sample input for username and password

--- iRacingData/iRacingData/Program.cs
+++ iRacingData/iRacingData/Program.cs
@@ -1,10 +1,54 @@
 ﻿using iRacingData;
+using System.Text;
 using System.Text.Json;
+using System.Security.Cryptography;
 
 var CarJson = File.ReadAllText(@"C:\Users\zcall\car.json");
 var TrackJson = File.ReadAllText(@"C:\Users\zcall\track.json");
-Console.WriteLine("Hello, World!");
+Console.Write("Enter username: ");
+string username = Console.ReadLine();
+string password = "";
+Console.Write("Enter password: ");
+ConsoleKeyInfo keyInfo;
+
+do
+{
+    keyInfo = Console.ReadKey(true);
+    // Skip if Backspace or Enter is Pressed
+    if (keyInfo.Key != ConsoleKey.Backspace && keyInfo.Key != ConsoleKey.Enter)
+    {
+        password += keyInfo.KeyChar;
+        Console.Write("*");
+    }
+    else
+    {
+        if (keyInfo.Key == ConsoleKey.Backspace && password.Length > 0)
+        {
+            // Remove last charcter if Backspace is Pressed
+            password = password.Substring(0, (password.Length - 1));
+            Console.Write("\b \b");
+        }
+    }
+}
+
+// Stops Getting Password Once Enter is Pressed
+while (keyInfo.Key != ConsoleKey.Enter);
+string encodedPassword = FormatAPIPassword(username, password);
+var body = new { email = username, password = encodedPassword };
+Console.WriteLine();
+Console.WriteLine("---------------------------");
+Console.WriteLine("Welcome " + username + ",");
+Console.WriteLine("Encoded Password is : " + body.password);
+
 var track = JsonSerializer.Deserialize<Track>(TrackJson);
 var car = JsonSerializer.Deserialize<Car>(CarJson);
+
+static string FormatAPIPassword(string username, string password)
+{
+    using var sha256 = SHA256.Create();
+    var hashedPasswordAndEmailBytes = sha256.ComputeHash(Encoding.UTF8.GetBytes(password + username.ToLower()));
+    var _encodedPassword = System.Convert.ToBase64String(hashedPasswordAndEmailBytes);
+    return _encodedPassword;
+}
 
 Console.ReadKey();
